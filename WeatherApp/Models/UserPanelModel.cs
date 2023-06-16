338f52a91b95e34a2aa3886b95d9416029b81768
tycjantyc <92381357+tycjantyc@users.noreply.github.com>
2023-06-16 12:34:33 +0200

    Add files via upload

---
+++ WeatherApp/Models/UserPanelModel.cs
@@ -0,0 +1,13 @@
+ public class UserPanelModel
+{
+        public string miasto {get; set;}
+        public string temp { get; set; }
+        public string wind { get; set; }
+        public string pressure { get; set; }
+        public string humidity { get; set; }
+        public string sunrise { get; set; }
+        public string sunset { get; set; }
+        public string cloud_cover { get; set; }
+        public string rain_perc { get; set; }
+        public string description { get; set; }
+}
