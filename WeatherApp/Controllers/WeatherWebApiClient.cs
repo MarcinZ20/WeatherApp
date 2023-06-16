aa966f5e9fa5099e24345d713fcb7eb5300184f5
tycjantyc <92381357+tycjantyc@users.noreply.github.com>
2023-06-16 12:33:51 +0200

    Update WeatherWebApiClient.cs

--- WeatherApp/Controllers/WeatherWebApiClient.cs
+++ WeatherApp/Controllers/WeatherWebApiClient.cs
@@ -81,20 +81,74 @@ namespace WeatherApp.Controllers
                 if (weatherString != string.Empty)
                 {
                     var parsedObject = JObject.Parse(weatherString);    //Tworzymy JObject
-                    List<String> Temps = new List<String>();
+                    String Miasto = parsedObject["city"]["name"].ToString();
                     List<String> Dates = new List<String>();
+                    List<String> Temps = new List<String>();
                     foreach (var record in parsedObject["list"])
                     {
-
+                        
                         Temps.Add(record["main"]["temp"].ToString());
                         Dates.Add(record["dt_txt"].ToString());
 
                         HourlyModel temp = new HourlyModel();
+                        
+                        temp.miasto = Miasto;
                         temp.temp = record["main"]["temp"].ToString();
                         temp.data = record["dt_txt"].ToString();
 
                         weather.Add(temp);
                     }
+
+                    //weather = JsonConvert.DeserializeObject<List<HourlyModel>>(resultSequence); //Deserializujemy nasz JSON
+                }
+                // return weatherString + "  "+response.StatusCode +" klucz:"+ ApiKey + httpclient.BaseAddress;
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Błąd: " + e.Message);
+            }
+
+            return weather;
+        }
+
+        public static String Time(String time)
+        {
+
+            double unixTimeStamp = Convert.ToDouble(time);
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+            
+            return dateTime.ToString("hh:mm tt");
+        }
+
+        public async Task<UserPanelModel> GetWeatherUserPanelAsync()
+        {
+            UserPanelModel weather = new UserPanelModel();
+            string weatherString = string.Empty;
+            try
+            {
+                HttpResponseMessage response = await httpclient.GetAsync("?id=" + LocalizationID + "&APPID=" + ApiKey + "&units=metric");
+                if (response.IsSuccessStatusCode)
+                {
+                    weatherString = await response.Content.ReadAsStringAsync(); //odczytujemy odpowiedź
+                }
+
+                if (weatherString != string.Empty)
+                {
+                    var parsedObject = JObject.Parse(weatherString);    //Tworzymy JObject
+                    weather.miasto = parsedObject["name"].ToString();
+                    weather.temp = parsedObject["main"]["temp"].ToString();
+                    weather.wind = parsedObject["wind"]["speed"].ToString();
+                    weather.pressure = parsedObject["main"]["pressure"].ToString();
+                    weather.humidity = parsedObject["main"]["humidity"].ToString();
+
+                    weather.sunrise = Time(parsedObject["sys"]["sunrise"].ToString());
+                    weather.sunset = Time(parsedObject["sys"]["sunset"].ToString());
+                    weather.cloud_cover = parsedObject["clouds"]["all"].ToString();
+                    weather.rain_perc = parsedObject["rain"]["1h"].ToString();
+                    weather.description = parsedObject["weather"]["main"].ToString() + " - " + parsedObject["weather"]["description"].ToString();
+                    
 
                     //weather = JsonConvert.DeserializeObject<List<HourlyModel>>(resultSequence); //Deserializujemy nasz JSON
                 }
