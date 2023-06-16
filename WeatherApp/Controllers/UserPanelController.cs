05f48174abcd5ccbc3b634289f2a76599fcdc93d
tycjantyc <92381357+tycjantyc@users.noreply.github.com>
2023-06-16 12:33:26 +0200

    Update UserPanelController.cs

--- WeatherApp/Controllers/UserPanelController.cs
+++ WeatherApp/Controllers/UserPanelController.cs
@@ -13,6 +13,29 @@ namespace WeatherApp.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            UserPanelModel model =  Database_controller.ListUserPanel("Warsaw");
+
+            List<HourlyModel> mod = Database_controller.ListHourly("Warsaw");
+
+
+            ViewData["T"] = model.temp;
+            ViewData["W"] = model.wind;
+            ViewData["P"] = model.pressure;
+            ViewData["H"] = model.humidity;
+            ViewData["sunrise"] = model.sunrise;
+            ViewData["sunset"] = model.sunset;
+            ViewData["cloud"] = model.cloud_cover;
+            ViewData["rain"] = model.rain_perc;
+            ViewData["des"] = model.description;
+
+            ViewData["miasto"]  =model.miasto;
+
+            ViewData["today"] = mod[1].temp;
+            ViewData["tom"] = mod[9].temp;
+            ViewData["atom"] = mod[17].temp;
+
+            ViewData["czas"] = DateTime.Now.ToString("hh:mm tt");
+            ViewData["data"] = DateTime.Now.ToString("dd/MM/yyyy");
             return View();
         }
     }
