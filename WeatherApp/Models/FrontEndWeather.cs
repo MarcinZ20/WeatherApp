288ec7f95fecec0f8a8221ef778262cc07ae306a
tycjantyc <92381357+tycjantyc@users.noreply.github.com>
2023-06-16 08:47:17 +0200

    Add files via upload

---
+++ WeatherApp/Models/FrontEndWeather.cs
@@ -0,0 +1,8 @@
+ public class FrontEndWeather
+{
+        public string MiastoID { get; set; }
+        public string Temp { get; set; }
+        public string Feels { get; set; }
+        public string Pressure { get; set; }
+        public string Humidity { get; set; }
+}
---
+++ WeatherApp/Models/WeatherModel.cs
@@ -0,0 +1,9 @@
+ public class WeatherModel
+{
+        public string temp { get; set; }
+        public string feels_like { get; set; }
+        public string temp_min { get; set; }
+        public string temp_max { get; set; }
+        public string pressure { get; set; }
+        public string humidity { get; set; }
+}
